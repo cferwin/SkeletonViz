dd778747d6cd8d899d5aca20816d6a3c7f5973d0
Christopher Erwin <chrisferwin@gmail.com>
2017-01-29 21:18:35 -0800

    Add mapping image to 3D plane.

--- SkeletonViz.cxx
+++ SkeletonViz.cxx
@@ -9,35 +9,62 @@
 #include <vtkRenderer.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
 #include <vtkCamera.h>
+#include <vtkTexture.h>
+#include <vtkPlaneSource.h>
 
 int main(int argc, char **argv) {
+	// ITK definitions for handling the slice image
 	typedef itk::Image<unsigned short, 2> ImageType;
 	typedef itk::ImageFileReader<ImageType> ReaderType;
 	typedef itk::ImageToVTKImageFilter < ImageType> ConnectorType;
 	ReaderType::Pointer reader = ReaderType::New();
 	ConnectorType::Pointer connector = ConnectorType::New();
 
+	// Import the ITK slice image
 	if (argc < 2) {
 		cout << "Include an image path as argument 1." << endl;
 		return 1;
 	}
 	reader->SetFileName(argv[1]);
 	
+	// Convert to VTK image
 	connector->SetInput(reader->GetOutput());
-	
-	vtkImageViewer *viewer = vtkImageViewer::New();
-	vtkRenderWindowInteractor *renderWindowInteractor = vtkRenderWindowInteractor::New();
-
 	connector->Update();
-	viewer->SetupInteractor(renderWindowInteractor);
-	viewer->SetInputData(connector->GetOutput());
-	viewer->SetSize(512, 512);
-	viewer->Render();
-	viewer->SetColorWindow(255);
-	viewer->SetColorLevel(128);
 
-	renderWindowInteractor->Start();
+	// Plug the image into a texture
+	vtkTexture *sliceTexture = vtkTexture::New();
+	sliceTexture->SetInputData(connector->GetOutput());
+	sliceTexture->InterpolateOn();
+	sliceTexture->Update();
+
+	// Set up a plane for the slice texture
+	vtkPlaneSource *plane = vtkPlaneSource::New();
+	vtkPolyDataMapper *planeMapper = vtkPolyDataMapper::New();
+	planeMapper->SetInputConnection(plane->GetOutputPort());
+	vtkActor *planeActor = vtkActor::New();
+	planeActor->SetMapper(planeMapper);
+	planeActor->SetTexture(sliceTexture);
+
+	// Set up VTK renderer
+	vtkRenderer *ren = vtkRenderer::New();
+	vtkRenderWindow *renWin = vtkRenderWindow::New();
+	renWin->AddRenderer(ren);
+	vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+	vtkInteractorStyleTrackballCamera *istyle = vtkInteractorStyleTrackballCamera::New();
+	iren->SetRenderWindow(renWin);
+	iren->SetInteractorStyle(istyle);
+
+	// Add plane actor to the scene
+	ren->AddActor(planeActor);
+	ren->SetBackground(0.1, 0.2, 0.4);
+	renWin->SetSize(512, 512);
+
+	// Render the scene
+	ren->ResetCamera();
+	renWin->Render();
+	iren->Start();
 
 	return 0;
 }

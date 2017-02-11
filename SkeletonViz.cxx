498c72f820f9ffa1258be4120136165a9ebb0c8b
Christopher Erwin <chrisferwin@gmail.com>
2017-02-10 19:56:41 -0800

    WIP. Add a single clipping plane widget and callback. Need to clean up.

--- SkeletonViz.cxx
+++ SkeletonViz.cxx
@@ -40,6 +40,9 @@
 #include <vtkUnstructuredGrid.h>
 #include <vtkPlaneWidget.h>
 #include <vtkPlane.h>
+#include <vtkBoxClipDataSet.h>
+#include <vtkAlgorithm.h>
+#include <vtkTrivialProducer.h>
 
 // Define a callback for transforming the clipping plane box
 // (From Examples\Tutorial\Cone6
@@ -49,28 +52,81 @@ public:
 	static ClippingBoxInteractionCallback *New() {
 		ClippingBoxInteractionCallback *c = new ClippingBoxInteractionCallback;
 		c->planes = vtkPlanes::New();
+		c->plane = vtkPlane::New();
+		//c->normals = vtkDataArray::New();
 
 		return c;
 	}
 
 	void Execute(vtkObject *caller, unsigned long vtkNotUsed(eventId), void* vtkNotUsed(callData)) VTK_OVERRIDE {
 		// Update the mapper's clipping planes with the new box widget planes.
-		widget = reinterpret_cast<vtkBoxWidget*>(caller);
+		widget = reinterpret_cast<vtkPlaneWidget*>(caller);
 
+		/*
+		// TODO:
+		// Flip the normals to make the box bound the visible volume
 		widget->GetPlanes(planes);
+		normals = planes->GetNormals();
+		
 		mapper->RemoveAllClippingPlanes();
+		mapper->SetClippingPlanes(planes);
+		planes->SetNormals(planes->GetNormals());
+		*/
+		/*
 		mapper->AddClippingPlane(planes->GetPlane(1));		// Back plane
-		//mapper->AddClippingPlane(planes->GetPlane(2));
-		//mapper->AddClippingPlane(planes->GetPlane(3));
+		mapper->AddClippingPlane(planes->GetPlane(2));
+		mapper->AddClippingPlane(planes->GetPlane(3));
 		mapper->AddClippingPlane(planes->GetPlane(4));		// Bottom plane
 		//mapper->AddClippingPlane(planes->GetPlane(5));
 		//mapper->AddClippingPlane(planes->GetPlane(6));
+		*/
+
+		// TEST: BOX CLIP
+		
+		//widget->GetPlanes(planes);
+		/*
+		clipper->GenerateClipScalarsOn();
+		clipper->SetBoxClip(planes->GetPlane(0)->GetNormal(),
+			planes->GetPlane(0)->GetOrigin(),
+			planes->GetPlane(1)->GetNormal(),
+			planes->GetPlane(1)->GetOrigin(),
+			planes->GetPlane(2)->GetNormal(),
+			planes->GetPlane(2)->GetOrigin(),
+			planes->GetPlane(3)->GetNormal(),
+			planes->GetPlane(3)->GetOrigin(),
+			planes->GetPlane(4)->GetNormal(),
+			planes->GetPlane(4)->GetOrigin(),
+			planes->GetPlane(5)->GetNormal(),
+			planes->GetPlane(5)->GetOrigin());
+		*/
+
+		// TEST: Clip Volume
+		/*
+		clipper->SetClipFunction(planes);
+		clipper->Update();
+		*/
+		//mapper->SetClippingPlanes(planes);
+		// TODO: Planes 0 and 3 are perpendicular to eachother, but both clip as infinite planes
+		//		 Add ARGV arguments for plane numbers and test permutations for a good structure,
+		//		 or figure out how to define two easily controllable PLANE WIDGETS.
+		//mapper->AddClippingPlane(planes->GetPlane(0));
+		//mapper->AddClippingPlane(planes->GetPlane(3));
+
+		// TEST: SINGLE CLIP PLANE
+		//mapper->RemoveAllClippingPlanes();
+		widget->GetPlane(plane);
+		mapper->AddClippingPlane(plane);
+
 		mapper->Update();
 	}
 
-	vtkBoxWidget *widget;
+	//vtkBoxWidget *widget;
+	vtkPlaneWidget *widget;
+	vtkPlane *plane;
 	vtkFixedPointVolumeRayCastMapper *mapper;
 	vtkPlanes *planes;
+	vtkDataArray *normals;
+	vtkClipVolume *clipper;
 };
 
 int main(int argc, char **argv) {
@@ -122,6 +178,13 @@ int main(int argc, char **argv) {
 	vtkVolume *volume = vtkVolume::New();
 	connector->SetInput(reader->GetOutput());
 
+	// Create a dummy producer to provide an output port from the connector, which
+	// seems to have no support for an output port
+	vtkTrivialProducer *connectorPort = vtkTrivialProducer::New();
+	connectorPort->SetOutput(connector->GetOutput());
+	connector->Update();
+	connectorPort->Update();
+
 	// Set up VTK renderer
 	vtkRenderer *ren = vtkRenderer::New();
 	vtkRenderWindow *renWin = vtkRenderWindow::New();
@@ -133,9 +196,13 @@ int main(int argc, char **argv) {
 
 	// Add volume mapper
 	vtkFixedPointVolumeRayCastMapper *mapper = vtkFixedPointVolumeRayCastMapper::New();
-	connector->Update();
 	
+	// Add a clipping box
 	mapper->SetInputData(connector->GetOutput());
+	vtkClipVolume *clipVolume = vtkClipVolume::New();
+	clipVolume->SetInputData(connector->GetOutput());
+	clipVolume->SetInputConnection(connectorPort->GetOutputPort());
+
 	double spacing[3];
 	connector->GetOutput()->GetSpacing(spacing);
 
@@ -149,17 +216,41 @@ int main(int argc, char **argv) {
 	prop->SetIndependentComponents(true);
 	prop->SetInterpolationTypeToLinear();
 
+	
 	colorFun->AddRGBPoint(-3024, 0, 0, 0, 0.5, 0.0);
 	colorFun->AddRGBPoint(850, .55, .25, .15, 0.5, .92);
 	colorFun->AddRGBPoint(1100, .88, .60, .29, 0.33, 0.45);
 	colorFun->AddRGBPoint(1200, 1, .94, .95, 0.5, 0.0);
 	colorFun->AddRGBPoint(3071, .83, .66, 1, 0.5, 0.0);
 	
+	/*
 	opacityFun->AddPoint(-3024, 0, 0.5, 0.0);
 	opacityFun->AddPoint(550, 0, 0.5, 0.4);
-	opacityFun->AddPoint(900, 0.5, 0.5, 0.61);
-	opacityFun->AddPoint(1300, 0.9, 0.5, 0.0);
-	opacityFun->AddPoint(3071, 0.9, 0.5, 0.0);
+	opacityFun->AddPoint(900, 0.8, 0.5, 0.61);
+	opacityFun->AddPoint(1300, 1, 0.5, 0.0);
+	opacityFun->AddPoint(3071, 1, 0.5, 0.0);
+	*/
+	
+
+	/* my color
+	colorFun->AddRGBPoint(1200, 1, 0.95, 0.95, 0.5, 1);
+	colorFun->AddRGBPoint(900, 0.7, 0.1, 0.15, 0.5, 0.92);
+	colorFun->AddRGBPoint(600, 0.1, 0.1, 0.8, 0.5, 0.9);
+	colorFun->AddRGBPoint(-3024, 0, 0, 0, 0.5, 0.0);
+	*/
+
+	/* TRY 2
+	opacityFun->AddPoint(600, 0.2, 0.5, 1);
+	opacityFun->AddPoint(900, 0.9, 0.5, 1);
+	opacityFun->AddPoint(1200, 1, 0.7, 1);
+	*/
+	opacityFun->AddPoint(400, 0.2, 0.5, 0);
+	opacityFun->AddPoint(700, 0.8, 0.5, 0.5);
+	opacityFun->AddPoint(800, 0.5, 0.5, 0.5);
+	//opacityFun->AddPoint(1200, 0.5, 0.5, 0.5);
+	//opacityFun->AddPoint(3000, 1, 0.5, 0.5);
+	opacityFun->AddPoint(1200, 0.3, 0.5, 1);
+	opacityFun->AddPoint(300, 1, 0.5, 1);
 	
 	gradientFun->AddPoint(-3024, 0, 0.5, 0.0);
 	gradientFun->AddPoint(100, 0, 0.5, 0.0);
@@ -177,6 +268,66 @@ int main(int argc, char **argv) {
 	planeWidget->GetPlane(plane);
 	*/
 
+	// Connect the volume to property and mapper
+	volume->SetProperty(prop);
+	volume->SetMapper(mapper);
+
+	// Create a box widget to control the clipping planes
+	/*
+	vtkBoxWidget *boxWidget = vtkBoxWidget::New();
+	boxWidget->SetInteractor(iren);
+	boxWidget->SetPlaceFactor(0.25);
+	boxWidget->SetProp3D(volume);
+	boxWidget->PlaceWidget();
+	boxWidget->On();
+	*/
+
+	// Set the clipping box to use the box widget
+	/*
+	vtkPlanes *planes = vtkPlanes::New();
+	boxWidget->GetPlanes(planes);
+	vtkPlane *p = planes->GetPlane(1);
+	*/
+	//clipVolume->GenerateClipScalarsOn();
+	/*
+	boxClip->SetBoxClip(planes->GetPlane(0)->GetNormal(),
+	planes->GetPlane(0)->GetOrigin(),
+	planes->GetPlane(1)->GetNormal(),
+	planes->GetPlane(1)->GetOrigin(),
+	planes->GetPlane(2)->GetNormal(),
+	planes->GetPlane(2)->GetOrigin(),
+	planes->GetPlane(3)->GetNormal(),
+	planes->GetPlane(3)->GetOrigin(),
+	planes->GetPlane(4)->GetNormal(),
+	planes->GetPlane(4)->GetOrigin(),
+	planes->GetPlane(5)->GetNormal(),
+	planes->GetPlane(5)->GetOrigin());
+	*/
+	//clipVolume->SetClipFunction(planes);
+	// !!!
+	//clipVolume->Update();
+
+	// TEST: Set up a clipping plane
+	vtkPlaneWidget *clipPlane = vtkPlaneWidget::New();
+	clipPlane->SetInteractor(iren);
+	clipPlane->SetPlaceFactor(1.25);
+	clipPlane->SetProp3D(volume);
+	clipPlane->PlaceWidget();
+	clipPlane->On();
+
+	// Set a callback for interaction
+	ClippingBoxInteractionCallback *callback = ClippingBoxInteractionCallback::New();
+	//callback->widget = boxWidget;
+	callback->widget = clipPlane;
+	callback->mapper = mapper;
+	callback->clipper = clipVolume;
+	//boxWidget->GetPlanes(callback->planes);
+	//boxWidget->AddObserver(vtkCommand::InteractionEvent, callback);
+	clipPlane->AddObserver(vtkCommand::InteractionEvent, callback);
+
+	// !!!
+	//mapper->SetInputConnection(clipVolume->GetOutputPort());
+	mapper->SetInputConnection(connectorPort->GetOutputPort());
     mapper->SetBlendModeToComposite();
 	prop->ShadeOn();
 	prop->SetAmbient(0.1);
@@ -188,25 +339,7 @@ int main(int argc, char **argv) {
 	prop->SetGradientOpacity(gradientFun);
 	prop->SetScalarOpacity(opacityFun);
 
-	// Connect the volume to property and mapper
-	volume->SetProperty(prop);
-	volume->SetMapper(mapper);
-
-	// Create a box widget to control the clipping planes
-	vtkBoxWidget *boxWidget = vtkBoxWidget::New();
-	boxWidget->SetInteractor(iren);
-	boxWidget->SetPlaceFactor(0.25);
-	boxWidget->SetProp3D(volume);
-	boxWidget->PlaceWidget();
-	boxWidget->On();
-
-	// Set a callback for interaction
-	ClippingBoxInteractionCallback *callback = ClippingBoxInteractionCallback::New();
-	callback->widget = boxWidget;
-	callback->mapper = mapper;
-	boxWidget->GetPlanes(callback->planes);
-	boxWidget->AddObserver(vtkCommand::InteractionEvent, callback);
-
+	
 	// Render the scene
 	ren->SetBackground(0.1, 0.2, 0.4);
 	renWin->SetSize(512, 512);
@@ -220,7 +353,8 @@ int main(int argc, char **argv) {
 	gradientFun->Delete();
 	opacityFun->Delete();
 	colorFun->Delete();
-	boxWidget->Delete();
+	//boxWidget->Delete();
+	clipPlane->Delete();
 	mapper->Delete();
 	volume->Delete();
 	iren->Delete();
